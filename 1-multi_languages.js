707ab264cdc3d7cdd17596813d446483809481d7
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-10-19 18:47:44 +0100

    Fix formatting and add new lines in multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,2 +1,5 @@
-javascript console.log("C is fun"); console.log("Python is cool");
-console.log("JavaScript is amazing");
+guillaume@ubuntu:~/0x12$ node 1-multi_languages.js 
+C is fun
+Python is cool
+JavaScript is amazing
+guillaume@ubuntu:~/0x12$ 
