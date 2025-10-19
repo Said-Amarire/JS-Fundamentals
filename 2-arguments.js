8e85d1cd8f7146fe2f4fd749a686a8c9c8fe3e9e
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-10-19 18:53:22 +0100

    Refactor argument handling in 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,1 +1,7 @@
-javascript const arg = process.argv[2]; console.log(arg ? arg : "No argument");
+if (process.argv.length <= 2) {
+  console.log("No argument");
+} else if (process.argv.length === 3) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
