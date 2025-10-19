53c6798172b98fed6b74bf43614e069af12af5b5
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-10-19 18:49:58 +0100

    Format console log statements for readability

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,1 +1,3 @@
-javascript console.log("C is fun"); console.log("Python is cool"); console.log("JavaScript is amazing");
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
