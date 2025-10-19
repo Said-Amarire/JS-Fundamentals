3d62f5746ebb01296043323e0540ca46c72cdaa5
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-10-19 18:48:31 +0100

    Convert shell output to console.log statements
    Replaced shell commands with JavaScript console logs.

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,5 +1,1 @@
-guillaume@ubuntu:~/0x12$ node 1-multi_languages.js 
-C is fun
-Python is cool
-JavaScript is amazing
-guillaume@ubuntu:~/0x12$ 
+javascript console.log("C is fun"); console.log("Python is cool"); console.log("JavaScript is amazing");
